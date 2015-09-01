6a37e007a19de4b31762d789f299213aab9e0689
cqbjk <claudio@quabblejack.com>
2015-09-01 07:18:06 -0400

    weather app. / bridge does not work / afnetworking file not found

---
+++ bridge.h
@@ -0,0 +1,15 @@
+
+//
+//  bridge.h
+//  web services app
+//
+//  Created by Quabblejack.com on 9/1/15.
+//  Copyright (c) 2015 CQ. All rights reserved.
+//
+
+#ifndef web_services_app_bridge_h
+#define web_services_app_bridge_h
+
+#import "AFNetworking/AFNetworking.h"
+
+#endif
--- web services app/ViewController.swift
+++ web services app/ViewController.swift
@@ -12,7 +12,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        //weather api call
+        let manager = AFHTTPRequestOperationManager()
+        
+        manager.GET( "http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=1",
+            parameters: nil,
+            success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+                println("Response: " + responseObject.description)
+                if let listOfDays = responseObject["list"] as? NSArray {
+                    if let tomorrow = listOfDays[0] as? NSDictionary {
+                        if let tomorrowsWeather = tomorrow["weather"] as? NSArray {
+                            if let firstWeatherOfDay = tomorrowsWeather[0] as? NSDictionary {
+                                if let forecast = firstWeatherOfDay["description"] as? String {
+                                    self.forecastLabel.text = forecast
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                println("Error: " + error.localizedDescription)
+        })
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
