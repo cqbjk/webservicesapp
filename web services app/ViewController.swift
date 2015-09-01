4089c965e56f7af3bf8da5ef7f80bbf9d495c938
cqbjk <claudio@quabblejack.com>
2015-09-01 00:12:25 -0400

    Initial Commit

---
+++ web services app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  web services app
+//
+//  Created by Quabblejack.com on 9/1/15.
+//  Copyright (c) 2015 CQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
